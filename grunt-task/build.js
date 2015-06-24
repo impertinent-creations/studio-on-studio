cfc9cfa99659f13394dc1e309952e97256112650
@rafszul <rafszul@gmail.com>
2015-06-25 00:01:55 +0100

    mods

--- grunt-task/build.js
+++ grunt-task/build.js
@@ -3,14 +3,14 @@ module.exports = function (grunt) {
 	 * Assets creation and minification
 	 */
 
-	//Builds all in one css app file 
+	//Builds all in one css app file
 	grunt.registerTask('buildCss', 'Minify All css', [
 		'clean:cssmin',
 		'concat:css',
 		'cssmin:minify'
 	]);
 
-	//Builds external libraries minified js 
+	//Builds external libraries minified js
 	grunt.registerTask('buildlib', 'Create lib.min.js asset', [
 		'clean:lib',
 		'concat:lib',
